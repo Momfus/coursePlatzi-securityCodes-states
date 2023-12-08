3c1b9ced6f50f358c99625910724416ce3c68ee3
Julian Muñoz Velázquez <momfus@outlook.com>
2023-12-08 20:24:28 -0300

    Security code with simple state added

--- src/UseState.js
+++ src/UseState.js
@@ -1,15 +1,26 @@
 import React from "react";
 
+const SECURITY_CODE = "paradigma";
+
 // Aca se hará sin usars React.Component
 function UseState({ name }) {
   const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
+
+  // Este estado será dinámico de lo que se escribe en el input
+  const [value, setValue] = React.useState("");
 
   // Recordar: gracias a virtual Dom, react solo renderiza los casos que haya cambiado el estado (no todo)
 
   React.useEffect(() => {
     if (loading) {
       setTimeout(() => {
+        if (value === SECURITY_CODE) {
+          setError(false);
+        } else {
+          setError(true);
+        }
+
         setLoading(false);
       }, 2000);
     }
@@ -23,7 +34,13 @@ function UseState({ name }) {
       {error && <p>Error: El código es incorrecto</p>}
       {loading && <p>Cargando...</p>}
 
-      <input placeholder="Código de seguridad" />
+      <input
+        placeholder="Código de seguridad"
+        value={value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+      />
       <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   );
