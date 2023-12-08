fca9ea37d6642113072a41df2458385a374f36cc
Julian Muñoz Velázquez <momfus@outlook.com>
2023-12-08 19:03:53 -0300

    useEffect in UseState

--- src/UseState.js
+++ src/UseState.js
@@ -3,6 +3,17 @@ import React from "react";
 // Aca se hará sin usars React.Component
 function UseState({ name }) {
   const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  // Recordar: gracias a virtual Dom, react solo renderiza los casos que haya cambiado el estado (no todo)
+
+  React.useEffect(() => {
+    if (loading) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    }
+  }, [loading]);
 
   return (
     <div>
@@ -10,9 +21,10 @@ function UseState({ name }) {
       <p>Por favor, escribe el código de seguridad.</p>
 
       {error && <p>Error: El código es incorrecto</p>}
+      {loading && <p>Cargando...</p>}
 
       <input placeholder="Código de seguridad" />
-      <button onClick={() => setError(!error)}>Comprobar</button>
+      <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   );
 }
