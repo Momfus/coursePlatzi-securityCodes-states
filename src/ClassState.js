35824c55ed4ced6700630b0c358093a71b7281d3
Julian Muñoz Velázquez <momfus@outlook.com>
2023-12-08 21:02:49 -0300

    input value and loading control added in classState

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Loading } from "./loading";
+
+const SECURITY_CODE = "paradigma";
+
 /**
  * Extender de React.Component es necesario para crear un componente de clase en React.
  * Esto te proporciona características como el estado del componente y los métodos del ciclo de
@@ -10,6 +13,7 @@ class ClassState extends React.Component {
     super(); // Con esto hacemos que se inicializa lo del padre primero (de React.component viene por defecto el setState y las convenciones que se hacen)
     // En las que son de clases, asi seteamos los estados
     this.state = {
+      value: "",
       error: false,
       loading: false,
     };
@@ -31,26 +35,39 @@ class ClassState extends React.Component {
 
     if (this.state.loading) {
       setTimeout(() => {
+        if (this.state.value !== SECURITY_CODE) {
+          this.setState({ error: true });
+        }
+
         this.setState({ loading: false });
       }, 2000);
     }
   }
 
   render() {
+    //const {error, loading, value} = this.state; // Por si no queremos estar haciendo cada rato el "this.state.blabla"
     return (
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor, escribe el código de seguridad.</p>
 
-        {this.state.error && <p>Error: El código es incorrecto</p>}
+        {this.state.error && !this.state.loading && (
+          <p>Error: El código es incorrecto</p>
+        )}
 
         {this.state.loading && <Loading />}
 
-        <input placeholder="Código de seguridad" />
+        <input
+          placeholder="Código de seguridad"
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({ value: event.target.value });
+          }}
+        />
         <button
           onClick={() =>
             // this.setState((prevState) => ({ error: !prevState.error })) // Formas de obtener el estado previo de un state
-            this.setState((prevState) => ({ loading: true }))
+            this.setState((prevState) => ({ error: false, loading: true }))
           }
         >
           Comprobar
