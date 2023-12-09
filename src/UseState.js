caf326b7947284f2727e4cbc37f970eb5fc7c73b
Julian Muñoz Velázquez <momfus@outlook.com>
2023-12-08 21:27:17 -0300

    Compound state added

--- src/UseState.js
+++ src/UseState.js
@@ -4,45 +4,48 @@ const SECURITY_CODE = "paradigma";
 
 // Aca se hará sin usars React.Component
 function UseState({ name }) {
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
-
-  // Este estado será dinámico de lo que se escribe en el input
-  const [value, setValue] = React.useState("");
+  // Con los estados compuestos podemos definir que valor del conjunto cambiar todo junto o uno a la vez
+  const [state, setState] = React.useState({
+    value: "",
+    error: false,
+    loading: false,
+  });
 
   // Recordar: gracias a virtual Dom, react solo renderiza los casos que haya cambiado el estado (no todo)
 
   React.useEffect(() => {
-    if (loading) {
-      setTimeout(() => {
-        if (value !== SECURITY_CODE) {
-          setError(true);
-        }
-
-        setLoading(false);
-      }, 2000);
+    if (state.loading) {
+      if (state.loading) {
+        setTimeout(() => {
+          setState((prevState) => {
+            if (prevState.value !== SECURITY_CODE) {
+              return { ...prevState, error: true, loading: false };
+            }
+            return { ...prevState, loading: false };
+          });
+        }, 2000);
+      }
     }
-  }, [loading]);
+  }, [state.loading]);
 
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor, escribe el código de seguridad.</p>
 
-      {error && !loading && <p>Error: El código es incorrecto</p>}
-      {loading && <p>Cargando...</p>}
+      {state.error && !state.loading && <p>Error: El código es incorrecto</p>}
+      {state.loading && <p>Cargando...</p>}
 
       <input
         placeholder="Código de seguridad"
-        value={value}
+        value={state.value}
         onChange={(event) => {
-          setValue(event.target.value);
+          setState({ ...state, value: event.target.value });
         }}
       />
       <button
         onClick={() => {
-          setError(false);
-          setLoading(true);
+          setState({ ...state, error: false, loading: true });
         }}
       >
         Comprobar
